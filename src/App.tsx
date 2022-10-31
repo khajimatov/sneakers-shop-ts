24b621c4d5d370b3d56d9dda31edb329d06d6f86
khajimatov <for.official.bkh@gmail.com>
2022-10-31 11:35:24 +0600

    Added Favorites and Orders page

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header';
+
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import Favorites from './pages/Favorites';
+import Orders from './pages/Orders';
 
 function App() {
   return (
@@ -17,6 +20,13 @@ function App() {
         <Route path="cart" element={<Suspense fallback={<div>Loading cart...</div>}>
           <Cart />
         </Suspense>} />
+        <Route path="favorites" element={<Suspense fallback={<div>Loading favorites...</div>}>
+          <Favorites />
+        </Suspense>} />
+        <Route path="orders" element={<Suspense fallback={<div>Loading orders...</div>}>
+          <Orders />
+        </Suspense>} />
+        <Route path="*" element={<strong>404: NOT FOUND</strong>} />
       </Routes>
     </div>
   );
