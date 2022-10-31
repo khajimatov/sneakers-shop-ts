f271b424f27e3082b916a0533317dd7386009834
khajimatov <for.official.bkh@gmail.com>
2022-10-31 17:40:33 +0600

    Changed some styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -71,6 +71,7 @@ const Home = () => {
                 <div className={styles.search}>
                     <label htmlFor="search">
                         <svg
+                            className={styles.searchIcon}
                             width="18"
                             height="18"
                             viewBox="0 0 16 16"
@@ -79,13 +80,13 @@ const Home = () => {
                         >
                             <path
                                 d="M15.25 15.25L11.8855 11.8795L15.25 15.25ZM13.75 7.375C13.75 9.06576 13.0784 10.6873 11.8828 11.8828C10.6873 13.0784 9.06576 13.75 7.375 13.75C5.68424 13.75 4.06274 13.0784 2.86719 11.8828C1.67165 10.6873 1 9.06576 1 7.375C1 5.68424 1.67165 4.06274 2.86719 2.86719C4.06274 1.67165 5.68424 1 7.375 1C9.06576 1 10.6873 1.67165 11.8828 2.86719C13.0784 4.06274 13.75 5.68424 13.75 7.375V7.375Z"
-                                stroke="#000000"
-                                strokeWidth="2"
+                                stroke="#c2c2c2"
+                                strokeWidth="1.5"
                                 strokeLinecap="round"
                             />
                         </svg>
                     </label>
-                    <input type="text" name="search" id="search" placeholder="Search sneakers" onChange={onChangeSearchInput} autoComplete="off" />
+                    <input className={styles.searchInput} type="text" name="search" id="search" placeholder="Search sneakers" onChange={onChangeSearchInput} autoComplete="off" />
                 </div>
             </div>
             <div className={styles.container}>
