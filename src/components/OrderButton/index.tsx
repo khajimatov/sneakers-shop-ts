84b531831ebfe615dbff44468701978ca6eecd1f
khajimatov <for.official.bkh@gmail.com>
2022-11-02 19:38:31 +0600

    ADD: Total price, order button on cart page

---
+++ src/components/OrderButton/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './OrderButton.module.css';
+
+const OrderButton = () => {
+    return (
+        <button className={styles.buyButton} >ORDER</button>
+    )
+}
+
+export default OrderButton;
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Cart.module.css';
 
 import { useSelector } from 'react-redux';
@@ -6,18 +6,26 @@ import { RootState } from '../../index';
 
 import Card from '../../components/Card';
 import Empty from '../../components/Empty';
+import OrderButton from '../../components/OrderButton';
 
 const Cart: React.FC = () => {
+
     const cart = useSelector((state: RootState) => state.cart);
 
     const renderItems = () => {
-        return cart.length > 0 ? cart.map(item => <Card key={item.id} id={item.id} title={item.title} price={item.price} imageURL={item.imageURL} />) : <Empty title='Cart is empty' />
+        return cart.length > 0 ? cart.map(item =>
+            <Card key={item.id} id={item.id} title={item.title} price={item.price} imageURL={item.imageURL} />
+        ) : <Empty title='Cart is empty' />
     }
 
     return (
         <div className={styles.cart}>
             <div className={styles.headingContainer}>
                 <h1 className={styles.heading}>Cart</h1>
+                <div>
+                    Total: $<b>{cart.length > 0 ? cart.map(o => o.price).reduce((a, c) => { return a + c }) : 0}</b> USD
+                    <OrderButton />
+                </div>
             </div>
             <div className={styles.container}>
                 {renderItems()}
