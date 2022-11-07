87a94e3970b151e202cf70c08159f0ad4ab04a92
khajimatov <for.official.bkh@gmail.com>
2022-11-07 16:11:43 +0600

    ADD: OrderCard component for each item in Order

--- src/components/OrderContainer/index.tsx
+++ src/components/OrderContainer/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import Card from '../Card';
 import styles from './Order.module.css';
 
 import { Order } from '../../types';
+import OrderCard from '../OrderCard';
 
 interface OrderContainerProps {
     item: Order
 }
 
-const OrderContainer = ({ item }: OrderContainerProps) => {
+const OrderContainer: React.FC<OrderContainerProps> = ({ item }) => {
     return (
         <div className={styles.orderContainer}>
             <h1>{item.index}</h1>
             <hr />
             <div className={styles.orderItems}>
-                {item.items.map(card => <Card key={card.id} id={card.id} title={card.title} price={card.price} imageURL={card.imageURL} />)}
+                {item.items.map(card => <OrderCard key={card.id} title={card.title} price={card.price} imageURL={card.imageURL} />)}
             </div>
         </div>
     )
