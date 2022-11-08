8b8a2634e505a9a96d93bf0655c89fb14e1aea5a
khajimatov <for.official.bkh@gmail.com>
2022-11-08 14:23:45 +0600

    ADD: postToOrders action

--- src/components/OrderButton/index.tsx
+++ src/components/OrderButton/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import { clearCart, postToOrders } from '../../store/actions';
+import { postToOrders } from '../../store/actions';
 import { useAppDispatch } from '../../store/hooks';
 import { Item } from '../../types';
 import styles from './OrderButton.module.css';
