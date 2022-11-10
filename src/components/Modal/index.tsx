a900c8233259626314ef9c9456d9948b36c0a826
khajimatov <for.official.bkh@gmail.com>
2022-11-10 16:34:31 +0600

    Added warning message

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -22,7 +22,7 @@ const Modal: React.FC<ModalProps> = ({ closeModal }) => {
     const [street, setStreet] = useState('');
     const [home, setHome] = useState('');
     const [isSubmitted, setIsSubmitted] = useState(false);
-
+    const [isWarn, setIsWarn] = useState(false);
 
     const handleBuyer = (event: React.ChangeEvent<HTMLInputElement>) => {
         setBuyer(event.target.value);
@@ -36,17 +36,21 @@ const Modal: React.FC<ModalProps> = ({ closeModal }) => {
     const handleHome = (event: React.ChangeEvent<HTMLInputElement>) => {
         setHome(event.target.value);
     }
-
     const onClickSubmit = async () => {
-        setIsSubmitted(true);
-        const date = Date.now();
+        
+        if (buyer && city && street && home) {
+            setIsSubmitted(true);
+            const date = Date.now();
 
-        const address: Address = { "city": city, "street": street, "home": home };
-        const newOrder: Order = { "index": "1", "items": items, "buyer": buyer, "date": date, "orderPrice": orderPrice, "address": address };
+            const address: Address = { "city": city, "street": street, "home": home };
+            const newOrder: Order = { "index": "1", "items": items, "buyer": buyer, "date": date, "orderPrice": orderPrice, "address": address };
 
-        await dispatch(postToOrders(newOrder));
-        setIsSubmitted(false);
-        navigate('/');
+            await dispatch(postToOrders(newOrder));
+            setIsSubmitted(false);
+            navigate('/');
+        } else {
+            setIsWarn(() => true);
+        }
     }
     const onClickCancel = () => {
         closeModal();
@@ -54,23 +58,24 @@ const Modal: React.FC<ModalProps> = ({ closeModal }) => {
     return (
         <div className={styles.modal}>
             <div className={styles.modalWindow}>
+                {isWarn && <div id='isWarn' className={styles.isWarn}>Fill in all fields</div>}
                 {isSubmitted ? <div className={styles.loader} ><img width={200} height={200} src="/img/loader.gif" alt="Loader GIF" /></div> :
                     <form autoComplete="off" name='orderForm' className={styles.orderForm}>
                         <ul>
                             <li>
-                                <label htmlFor="buyer">Name:</label>
+                                <label htmlFor="buyer">Name:<span>*</span></label>
                                 <input autoComplete="off" onChange={handleBuyer} type="text" name="buyer" id="buyer" />
                             </li>
                             <li>
-                                <label htmlFor="city">City:</label>
+                                <label htmlFor="city">City:<span>*</span></label>
                                 <input autoComplete="off" onChange={handleCity} type="text" name="city" id="city" />
                             </li>
                             <li>
-                                <label htmlFor="street">Street:</label>
+                                <label htmlFor="street">Street:<span>*</span></label>
                                 <input autoComplete="off" onChange={handleStreet} type="text" name="street" id="street" />
                             </li>
                             <li>
-                                <label htmlFor="home">Home:</label>
+                                <label htmlFor="home">Home:<span>*</span></label>
                                 <input autoComplete="off" onChange={handleHome} type="text" name="home" id="home" />
                             </li>
                         </ul>
