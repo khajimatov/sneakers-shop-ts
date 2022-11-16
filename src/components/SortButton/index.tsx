480ace28edd2655b4c6735d6b4003ddb8f2dba64
khajimatov <for.official.bkh@gmail.com>
2022-11-16 18:17:15 +0600

    Made SortButton look better

--- src/components/SortButton/index.tsx
+++ src/components/SortButton/index.tsx
@@ -1,19 +1,46 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { setItems } from '../../store/actions';
+import { useAppDispatch } from '../../store/hooks';
 import styles from './SortButton.module.scss'
 
 const SortButton = () => {
-    const [selectedOption, setSelectedOption] = useState('sort');
+    const [load, setLoad] = useState(false);
+    const dispatch = useAppDispatch();
 
-    const onOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectedOption(e.target.value)
+    const onOptionChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setLoad(true);
+        if (e.target.value === 'high') {
+            const { data } = await axios.get(`https://611a826e5710ca00173a1a6e.mockapi.io/items?sortby=price&order=desc`)
+            dispatch(setItems(data));
+        } else if (e.target.value === 'low') {
+            const { data } = await axios.get(`https://611a826e5710ca00173a1a6e.mockapi.io/items?sortby=price`)
+            dispatch(setItems(data));
+        }
+        setLoad(false);
     }
-    console.log(selectedOption);
+
     return (
-        <select onChange={onOptionChange} className={styles.sorting} name="sorting" id="sorting">
-            <option value="sort">Sorting</option>
-            <option value="low">Price: Low to High</option>
-            <option value="high">Price: High to Low</option>
-        </select>
+        <div className={styles.container}>
+            <select onChange={onOptionChange} className={styles.sorting} name="sorting" id="sorting">
+                <option value="sort">Sorting</option>
+                <option value="low">Price: Low to High</option>
+                <option value="high">Price: High to Low</option>
+            </select>
+            {load ?
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" className={styles.animate_spin}>
+                    <line x1="12" y1="2" x2="12" y2="6"></line>
+                    <line x1="12" y1="18" x2="12" y2="22"></line>
+                    <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
+                    <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
+                    <line x1="2" y1="12" x2="6" y2="12"></line>
+                    <line x1="18" y1="12" x2="22" y2="12"></line>
+                    <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
+                    <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
+                </svg>
+                : null}
+        </div>
+
     )
 }
 
