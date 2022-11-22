ced3f4d2d27002de1e8644929e562eff432f7e2c
khajimatov <for.official.bkh@gmail.com>
2022-11-22 16:51:07 +0600

    Added Toast component

---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Toast.module.scss';
+
+const Toast = () => {
+    return (
+        <div className={styles.toast}>Toast</div>
+    )
+}
+
+export default Toast;
