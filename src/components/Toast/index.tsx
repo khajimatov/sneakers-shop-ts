7aa734652b9cb0e4321539254a097f4eb99356b6
khajimatov <for.official.bkh@gmail.com>
2022-11-23 12:11:03 +0600

    Made Toast look better

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { RootState } from '../..';
+import { removeToast } from '../../store/actions';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import styles from './Toast.module.scss';
 
-const Toast = () => {
+const Toast: React.FC = () => {
+    const toastText = useAppSelector((state: RootState) => state.toastText);
+    const dispatch = useAppDispatch();
+
+    const onCloseToast = () => {
+        dispatch(removeToast());
+    }
+
     return (
-        <div className={styles.toast}>Toast</div>
+        <div className={styles.toast}>{toastText}<button onClick={() => onCloseToast()} >X</button></div>
     )
 }
 
