bd54cda53a90df9e64d3f9d80fc9200bb3763d78
khajimatov <for.official.bkh@gmail.com>
2022-11-23 11:42:22 +0600

    Moved Toast to the MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
 import { Outlet } from 'react-router-dom';
+import { RootState } from '..';
 
 import Header from '../components/Header';
+import Toast from '../components/Toast';
+import { removeToast } from '../store/actions';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
 
 const MainLayout: React.FC = () => {
+    const toastText = useAppSelector((state: RootState) => state.toastText);
+    const dispatch = useAppDispatch();
     return (
-            <div className="wrapper">
-                <Header />
-                <div className="content">
-                    <Outlet />
-                </div>
+        <div className="wrapper">
+            {toastText ? <Toast onClose={() => dispatch(removeToast())} /> : null}
+            <Header />
+            <div className="content">
+                <Outlet />
             </div>
+        </div>
     )
 }
 
